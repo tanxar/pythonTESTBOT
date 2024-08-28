ccbbefb4d72df15acd21f6bde046f5abdbb357d5
tanxar <153452260+tanxar@users.noreply.github.com>
2024-08-28 04:59:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 import logging
+from flask import Flask, request
+import os
+
+# Initialize Flask app
+app = Flask(__name__)
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -10,15 +15,24 @@ logger = logging.getLogger(__name__)
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text('Hello, World!')
 
-def main() -> None:
-    # Initialize the application with your bot token
-    application = Application.builder().token("7403620437:AAHUzMiWQt_AHAZ-PwYY0spVfcCKpWFKQoE").build()
+# Initialize Telegram bot application
+bot_token = "7403620437:AAHUzMiWQt_AHAZ-PwYY0spVfcCKpWFKQoE"
+application = Application.builder().token(bot_token).build()
+application.add_handler(CommandHandler("start", start))
 
-    # Register the start command handler
-    application.add_handler(CommandHandler("start", start))
+# Define the route for webhook
+@app.route(f'/{bot_token}', methods=['POST'])
+def webhook():
+    json_str = request.get_data(as_text=True)
+    update = Update.de_json(json_str, application.bot)
+    application.update_queue.put(update)
+    return 'OK'
 
-    # Start the Bot
-    application.run_polling()
+# Set webhook
+def set_webhook():
+    webhook_url = f"https://pythontestbot-f4g1.onrender.com/{bot_token}"
+    application.bot.set_webhook(url=webhook_url)
 
 if __name__ == '__main__':
-    main()
+    set_webhook()
+    app.run(host='0.0.0.0', port=8080)
